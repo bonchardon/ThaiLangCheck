37056d42ef4eb5a7f4d82d00e550f137d3b857af
Maryna_Boroda <marynaboroda123@gmail.com>
2022-04-19 18:52:51 +0300

    editing

--- main.py
+++ main.py
@@ -1,75 +1,37 @@
 # TODO: apply and test some tokenization approaches that could be applied when working with Thai language
 #  think of editing approaches when working with Thai, as well
+from xml.etree import ElementTree as ET
+
+import pythaispell
+import sentencepiece
+# import pythaipiece
 
 import pythainlp
 from pythainlp import sent_tokenize, word_tokenize
-from pythainlp.corpus.common import thai_words
-from pythainlp.tokenize.multi_cut import find_all_segment, mmcut, segment
-from pythainlp import Tokenizer
 from pythainlp.tokenize.multi_cut import find_all_segment, mmcut, segment
 from pythainlp.util import normalize
-from pythainlp import correct
 # for spell checking
 from pythainlp.spell import NorvigSpellChecker
 from pythainlp.corpus import ttc
-
+import pythainlp.word_vector
+from pythainlp.corpus.common import thai_words
+from pythainlp import Tokenizer
+from pythainlp import spell, correct
+from pythainlp import correct
+from pythainlp.tokenize import subword_tokenize
 
 import tltk
 from wordcut import Wordcut
-from nlpo3 import load_dict, segment
+from nlpo3 import segment as seg_nlpo3
+from nlpo3 import load_dict
+
 import deepcut
 import jpype
 import os
+import pandas as pd
 from py4j.java_gateway import JavaGateway
 gateway = JavaGateway()
 # from cutkum.tokenizer import Cutkum
-
-
-class Tokenization:
-
-    def __init__(self, data):
-
-        # normalize helps us to ensure there are no typos or other minor issues in text
-        # correct helps to correct spelling, it returns most likely spelling
-
-        self.dataa = normalize(data)
-        self.data = correct(self.dataa)
-
-    # TODO: here we try out all the possible ways to tokenize thai text
-
-    def pythainlp_try(self, data):
-
-        regular_tokenizer = word_tokenize(data, keep_whitespace=False)
-
-        words = set(thai_words())
-        custom_tokenizer = Tokenizer(words)
-        print("All possible segmentations using pythainlp ==> ", find_all_segment(data))
-        return "Regular pythainlp library check ==> ", regular_tokenizer, "Custom pythainlp library check ==> ", custom_tokenizer.word_tokenize(data)
-
-    def wordcutThai(self, data, input):
-        wordcut = Wordcut(data)
-        return wordcut.tokenize(input)
-
-    def ThaiNltk(self, data):
-        return tltk.corpus.collocates(data)
-
-    def Thaideepcut(self, data):
-        return deepcut.tokenize(data)
-
-    def Thainlpo3(self, data):
-
-        # here we can also load additional dictionaries and use those in segmentation process
-        # to make it more profound
-
-        return segment(data, "countries")
-
-    def cutkum_tokenizer(self, data):
-        " REMEMBER: cutkum works just fine, but unfortunately cutkum requires old versions of tensorflow \
-                    (tensorflow1.x) installed:\
-                    it puts tensorflow.contrib error if that a new version of tensorflow.\
-                    Other thai libraries require tensorflow2.x versions."
-        ck = Cutkum()
-        return ck.tokenize(data)
 
 
 class LexTo(object):
@@ -103,15 +65,52 @@ class LexTo(object):
         return wordList, typeList
 
 
-class SpellingCheck():
+class Tokenization:
+
+    # here are various thai libraries used to analyze thai
+    # (what actually interests us --> tokenization of a text with mistakes and typos)
+
+    # TODO: here we try out all the possible ways to tokenize thai text
+
+    def pythainlp_try(self, data):
+        # Default word tokenizer use a word list from pythainlp.corpus.common.thai_words()
+        words = set(thai_words())
+        custom_tokenizer = Tokenizer(words)
+        return find_all_segment(data), \
+               word_tokenize(data, keep_whitespace=False), \
+               custom_tokenizer.word_tokenize(data)
+
+    def wordcutThai(self, data):
+        wordcut = Wordcut.bigthai()
+        return wordcut.tokenize(data)
+
+    def ThaiNltk(self, data):
+        return tltk.corpus.collocates(data)
+
+    def Thaideepcut(self, data):
+        return deepcut.tokenize(data)
+
+    def Thainlpo3(self, data):
+
+        # here we can also load additional dictionaries and use those in segmentation process
+        # to make it more profound
+        return seg_nlpo3(data)
+
+    def cutkum_tokenizer(self, data):
+        " REMEMBER: cutkum works just fine, but unfortunately cutkum requires old versions of tensorflow \
+                    (tensorflow1.x) installed:\
+                    it puts tensorflow.contrib error if that a new version of tensorflow.\
+                    Other thai libraries require tensorflow2.x versions."
+        ck = Cutkum()
+        return ck.tokenize(data)
+
+
+class SpellingCheck:
 
     # TODO: here trying possible ways to check spelling without prior tokenization
 
     # def __init__(self, data):
     #     super().__init__(data)
-
-    def normalize(self, data):
-        return normalize(data)
 
     def spell_checker(self, data):
 
@@ -133,22 +132,35 @@ class FastT:
 
 if __name__ == '__main__':
 
+    path = "C:\/Users\Марина\Desktop\/trwiki-20181001-corpus.xml"
+    # path = False
+    if path:
+        tree = ET.parse(path)
+        root = tree.getroot()
+
+        for page in root.findall('page'):
+            print(page.find('content').text)
+
     train_data = "ข้อ 1 มนุษย์ทั้งหลายเกิดมามีอิสระและเสมอภาคกันในเกียรติศักด[เกียรติศักดิ์]และสิทธิ ต่างมีเหตุผลและมโนธรรม และควรปฏิบัติต่อกันด้วยเจตนารมณ์แห่งภราดรภาพ"
-    data = "สบายดีไหมเเหลืยม"
-    # data = data.encode(encoding='UTF-8')
+    data = "ผมลััักของนะผมลักของนะ"
+    # when applied typos are deleted
+    # data = normalize(data)
 
-    separateWords = Tokenization(data)
+    separateWords = Tokenization()
     spelling = SpellingCheck()
-
-    # TODO: check issue "jpype._jvmfinder.JVMNotFoundException: No JVM shared library file (jvm.dll) found. Try setting up the JAVA_HOME environment variable properly."
+    # TODO: check issue "jpype._jvmfinder.JVMNotFoundException: No JVM shared library file (jvm.dll) found.
+    #  Try setting up the JAVA_HOME environment variable properly."
     # lexto = LexTo()
     # words, types = lexto.tokenize(data)
     # print("Lexto progect check ==> ", words, types)
 
-    print(separateWords.pythainlp_try(data))
-    print("WordcutThai library check ==> ", separateWords.wordcutThai(data=train_data, input=data))
-    print("Deepcut library check ==> ", separateWords.Thaideepcut(data=data))
-    # print("nlpo3 library check ==> ", separateWords.Thainlpo3(data=data))
-    # print(separateWords.cutkum_tokenizer(data=data))
+    # just a dataframe with all initial result
 
-    print("Spell checking using pythainlp ==> ", spelling.spell_checker(data=data))
+    all, reg, cus = separateWords.pythainlp_try(data)
+
+    df = pd.DataFrame()
+    df['Possible segmentations using pythainlp'] = all
+    df['Regular pythainlp library'] = ['|'.join(reg)]
+    df['Custom pythainlp library'] = ['|'.join(cus)]
+    df['Deepcut library'] = '|'.join(separateWords.Thaideepcut(data=data))
+    print(df.T)
